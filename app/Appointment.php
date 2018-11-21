4729c69e33216670eedf778c57e14deecda91fac
AathmikaM <aathmikamurukiah15@gmail.com>
2018-11-22 04:40:43 +0530

    AppointmentController

--- app/Appointment.php
+++ app/Appointment.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Appointment extends Model
 {
-    
+    /*public function scopeSearch($query, $s) {
+        return $query->where('experience', 'like', '%' .$s. '%')
+            ->orWhere('description', 'like', '%' .$s. '%')
+            ->orWhere('address', 'like', '%' .$s. '%');
+    }
+
+    protected $table = 'advertisements';
+    protected $primaryKey = 'advertisement_id';
+    public $timestamps = false;
+    protected $fillable = [
+        'advertisement_id','type','title', 'description', 'bit_end_date', 'image',
+        'address', 'lat', 'lng','distance', 'price', 'status', 'user_id', 'category_id','sub_cat_id', 'sub_sub_cat_id','start_date',
+        'end_date','created_at','updated_at','zip','experience','user_name',
+    ];*/
 }
---
+++ app/Http/Controllers/AppointmentController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Appointment;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Input;
+
+class AppointmentController extends Controller
+{
+    public function accept(request $request, $id)
+    {
+        $hire = Appointment::find($id);
+        $hire->status = 'accepted';
+        $hire->save();
+        $request->session()->flash('alert-success', 'Accept successfully');
+        return Redirect::back();
+    }
+
+    public function cancel(request $request, $id)
+    {
+        $hire = Appointment::find($id);
+        $hire->status = 'cancelled';
+        $hire->save();
+        $request->session()->flash('alert-success', 'cancelled successfully');
+        return Redirect::back();
+    }
+
+    public function index()
+    {
+        $appoinments = Appointment::all();
+        return view('appoinments', ['appoinments' => $appoinments]);
+    }
+}
