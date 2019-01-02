be0b52f33cba8ea97bbaec8b0d1f9e6fb767f7cb
Your Name <chaminlasantha@gmail.com>
2019-01-02 12:47:39 +0530

    pet profile ui updated

--- resources/views/pet/petprofile.blade.php
+++ resources/views/pet/petprofile.blade.php
@@ -10,19 +10,13 @@
  
  
  <div class="card-header">
-    <ul class="nav nav-pills card-header-pills">
-      <li class="nav-item">
-        <a class="nav-link active" href="/petowners/{id}/{id}">Genaral-Treatments</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="/petowners/{id}/{id}/periodic">Periodic-Treatments</a>
-      </li>
-      
-    </ul>
+    
 
-<form>
+<form action="" >
  <div class="row">
     <div class="col-6"> 
+    <div class="card mb-5">
+        <div class ="card-header">
       <div class="py-5 text-center">
             <!-- add the href to get image -->
             <img class="rounded-circle" src="data:image/jpg;charset=utf8;base64,{{base64_encode($pet->image)}}"  alt="Generic placeholder image" width="140" height="140">
@@ -30,8 +24,9 @@
             <h3>Species:{{$pet->species}}</h3>
             <h4>Weight of The Pet:{{$pet->weight}}</h4>
             <h5>Age Of The Pet:{{$pet->age}}</h5>
-            
-         
+              
+            </div>
+          </div>
           </div>
           </div>
 
@@ -39,7 +34,7 @@
 <div class="col-6"> 
   <div class="form-group">
     <label for="exampleFormControlTextarea1">Special Notes</label>
-    <textarea class="form-control" id="exampleFormControlTextarea1" rows="20"></textarea>
+    <textarea class="form-control" id="exampleFormControlTextarea1" rows="27"></textarea>
     <div class="row">
     <div class="orm-control col-lg-2">
       <button type="submit" class="btn btn-primary">Save</button>
@@ -50,9 +45,17 @@
   </div>
   </div>
   </div>
+  
+
   </div>
   
-  
+  <div class="row">
+    <div class="col-6">
+    <button type="button" class="btn btn-primary btn-lg btn-block">General Treatment</button> 
+    </div>
+    <div class="col-6">
+    <button type="button" class="btn btn-primary btn-lg btn-block">Periodic Treatment</button> 
+    </div>
 
 
 
