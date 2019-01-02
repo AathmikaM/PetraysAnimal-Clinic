c16b0ebfe4ab3f4abaf675aff889d1537fb64caf
Your Name <chaminlasantha@gmail.com>
2019-01-02 12:07:03 +0530

    Pet profile updated

--- app/Http/Controllers/postToPets.php
+++ app/Http/Controllers/postToPets.php
@@ -10,24 +10,18 @@ class postToPets extends Controller
     public function show($pid){
         $pet=Pet::find($pid);
        // $medicines=Species::find($id);
-        return view('pet.petprofile',['pet'=>$pet,'treatments'=>$pet->treatments]);
+        return view('pet.petprofile',['pet'=>$pet]);
 
     }
 
     public function store(Request $request,Pet $pet){
     
         
-   
-      $treatment->title=$request->input('title');
-      $treatment->discription=$request->input('description');
-      $treatment->quantity=$request->input('quantity');
-      $treatment->medicine=$request->input('inputGroupSelect01');
-      $treatment->save();
+    
 
       
 
-      
-      return redirect('/create');
+
     
 
     }
--- resources/views/pet/petprofile.blade.php
+++ resources/views/pet/petprofile.blade.php
@@ -5,9 +5,9 @@
 
  @section('content')
 
-{!! Form::open(['action' => 'postToPets@store','method'=>'post']) !!}
 
 
+ 
  
  <div class="card-header">
     <ul class="nav nav-pills card-header-pills">
@@ -19,100 +19,55 @@
       </li>
       
     </ul>
-  
- <div class="container">
+
+ <div class="row">
+    <div class="col-6"> 
       <div class="py-5 text-center">
             <!-- add the href to get image -->
             <img class="rounded-circle" src="data:image/jpg;charset=utf8;base64,{{base64_encode($pet->image)}}"  alt="Generic placeholder image" width="140" height="140">
-            <h2 >{{$pet->name}} </h2>
-            <p>{{$pet->species}}</p>
-            <p>{{$pet->weight}}</p>
-            <p>{{$pet->age}}</p>
+            <h2 >Pet Name:{{$pet->name}} </h2>
+            <h3>Species:{{$pet->species}}</h3>
+            <h4>Weight of The Pet:{{$pet->weight}}</h4>
+            <h5>Age Of The Pet:{{$pet->age}}</h5>
             
+         
           </div>
           </div>
 
+
+<form id="main" method="post" action="{{route('pet/{id}/{pid}',$pet->id)}}">
+<div class="col-6"> 
   <div class="form-group">
-    {{Form::label('specialNote','Special Discription about pet')}}
-    {{Form::textarea('specialNote','',['class'=>'form-control','placeholder'=>'Note-Down Here '])}}
-</div>
-
-<div class="form-group">
-    {{Form::label('title','Title')}}
-    {{Form::text('title','',['class'=>'form-control','placeholder'=>'Title'])}}
-</div>
-
-
-<div class="form-group">
-    {{Form::label('specialNote','Discription about treatment')}}
-    {{Form::textarea('specialNote','',['class'=>'form-control','placeholder'=>'Note-Down Here '])}}
-</div>
-
-
-
-
- 
-{{Form::submit('Save',['class'=>'btn btn-primary','name'=>'save','value'=>'save'])}}
-
-
-    
-{!! Form::close() !!} 
-
-
-
-
-
-
-
-
-
-
-
-
-
-<br>
-<br>
-<br>
-
-<h1>Recently Added</h1>
-
-@if(count($treatments)>0)
-@foreach($treatments as $treatment)
-
-
-  <div class="card-header">
-  <h5 class="card-title">{{$treanment->title}}</h5>
+    <label for="exampleFormControlTextarea1">Special Notes</label>
+    <textarea class="form-control" id="exampleFormControlTextarea1" rows="20" value="{{$pet->specialnote}}"></textarea>
+    <div class="row">
+    <div class="orm-control col-lg-2">
+      <button type="submit" class="btn btn-primary">Save</button>
+    </div>
+    <div class="orm-control col-lg-0">
+      <button type="submit" class="btn btn-primary">Edit</button>
+    </div>
   </div>
-  <div class="card text-center">
-  <div class="card-body">
-    
-    <p class="card-text">{{$treanment->description}}</p>
-    <a href="#" class="btn btn-primary">Edit Treatment Reccord</a>
-    <a href="#" class="btn btn-primary">Delete Treatment Reccord</a>
   </div>
-  <div class="card-footer text-muted">
-  {{$treanment->created_at}}
   </div>
-</div>
-<br>
-<br>
+  </div>
+  
+  
 
 
-<!-- <div class="my-3 p-3 bg-white rounded box-shadow">
-        <div class="d-flex justify-content-between align-items-center w-100">
-              <strong class="text-gray-dark">{{$treanment->colour}}</strong>
-              <small>{{$treanment->species}}</small>
-            </div>
-</div>
 
-    <small>{{$treanment->species}}</small>
-<hr>
-<hr>
-     -->
 
-@endforeach
-</ul>
 
-@endif
-</div>
+
+
+
+
+
+
+
+
+
+
+
+
 @endsection 
