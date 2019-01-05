8fdc8060c36b10bbcea01fc037e8bcd861f78d4d
SahanBuddhika <sahan.bu.1994@gmail.com>
2019-01-05 14:59:27 +0530

    create password reset

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'buddhika.sa.1994@gmail.com'),
+        'name' => env('MAIL_FROM_NAME', 'PetRays Animal Clinic'),
     ],
 
     /*
@@ -100,6 +100,15 @@ return [
     */
 
     'sendmail' => '/usr/sbin/sendmail -bs',
+
+
+    'stream' =>[
+        'ssl'=>[
+            'allow_self_signed' => true,
+            'verify_peer' => false,
+            'verify_peer_name' => false,
+        ],
+    ],
 
     /*
     |--------------------------------------------------------------------------
