073f78831187c6b39a07025883a09a5e4111f4a4
Your Name <chaminlasantha@gmail.com>
2019-01-06 15:53:00 +0530

    isssued

--- app/issuedMedicine.php
+++ app/issuedMedicine.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class IssuedMedicine extends Model
+class ssuedMedicine extends Model
 {
     public function treatments(){
         return $this->belongsTo('App\Treatment');
